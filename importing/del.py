f8ed7bc93fa7c440995debed1d96bfd0d6b47123
umnoy <ikozloff8@gmail.com>
2025-06-13 22:13:57 +0300

    minor update

--- importing/del.py
+++
@@ -1,9 +0,0 @@
-from transformers.utils import cached_file
-import shutil
-import os
-
-cache_dir = os.path.join(os.path.expanduser("~"), ".cache", "huggingface", "transformers")
-model_dir = os.path.join(cache_dir, "models--facebook--wmt19-ru-en")
-
-# удаление модели
-shutil.rmtree(model_dir, ignore_errors=True)
--- importing/save_token.py
+++
@@ -1,4 +0,0 @@
-from transformers import AutoTokenizer
-
-tokenizer = AutoTokenizer.from_pretrained("facebook/mbart-large-50-many-to-many-mmt")
-tokenizer.save_pretrained("onnx/tokenizer")
